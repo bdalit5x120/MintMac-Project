8134be176401285a210a90c2a57cab1d73b5637c
Brandon Dalit <66238434+bdalit5x120@users.noreply.github.com>
2025-09-18 14:24:25 -0700

    Update sshgenerate.sh

--- Scripts/sshgenerate.sh
+++ Scripts/sshgenerate.sh
@@ -1,1 +1,11 @@
+#! /bin/bash
+# Create new hosts
 
+echo "Creating new host keys for SSH"
+sudo ssh-keygen -A
+
+echo "restarting the server"
+sudo systemctl enable ssh
+sudo systemctl restart ssh
+
+echo "Done"
